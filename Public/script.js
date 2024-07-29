b54d04b4e6a41c4035ba0d194415d8d6a69d88af
Dainsh1 <danishgakhar0910@icloud.com>
2024-07-28 22:48:09 -0400

    Add files via upload

--- Public/script.js
+++ Public/script.js
@@ -1,1 +1,17 @@
+document.getElementById("loadDataBtn").addEventListener("click", async () => { // by clicking the button this function fetches data from data.json file
+    const apiURL = 'http://localhost:3333/api/products';  // URL on which fetched data will be displayed
+    try {  // try and catch function helps in managing errors
+        const response = await fetch(apiURL);
+        const data = await response.json();  // asynchronous function
 
+        let ulContent = '<ul>';
+        for (const item of data) {
+            ulContent += `<li>${item.name}: $${item.price}<br>${item.description}</li>`;
+        }
+        ulContent += '</ul>';
+        
+        document.getElementById("dataContainer").innerHTML = ulContent; 
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+});
